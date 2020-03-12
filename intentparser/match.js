359079c8c2f55ba51b84b7cbb80c2f357066646f
Ben Bucksch <ben.bucksch@beonex.com>
2020-03-12 04:53:55 +0100

    Intent parser: Command parsing works

--- intentparser/match.js
+++ intentparser/match.js
@@ -78,44 +78,85 @@ export default class IntentParser {
     let app = intent.app;
     console.log("app", app.id, "command with placeholders:", command);
 
-    let args = {
-      song: inputText, // TODO only the variable text
-    }
+    // process variables
+    let args = getVariables(inputText, command, intent);
     for (let name in args) {
-      let validValues = await app.validVariableValues(name);
-      args[name] = matchVariable(args[name], validValues);
+      let dataType = intent.parameters[name];
+      if (dataType.finite) {
+        // normalize to allowed values
+        args[name] = matchVariable(args[name], dataType.terms);
+      }
+      args[name] = dataType.valueIDForTerm(args[name]);
     }
 
     try {
       // Start the app
       return await intent.run(args);
     } catch (ex) { // Exceptions should be caught by intent. This is a fallback.
+      console.error(ex);
       return ex.message || ex;
     }
   }
+}
 
-  getVariables(inputText, command, intent) {
-    // cut common start string
-
-  }
-
-  /**
-  * @param inputText {string} user input, only the part for this variable
-  * @param validValues {Array of string}
-  * @returns {string} one line from validValues, the closest match
+/**
+  * This is a simplistic attempt at matching the input string
+  * with its variables with the expected command string and its placeholders.
+  * HACK TODO Instead, need levenshtein with wildcards.
   */
-  matchVariable(inputText, validValues) {
-    if (!validValues) {
-      return inputText;
+function getVariables(inputText, command, intent) {
+  let args = {};
+  let inputWords = inputText.split(" ");
+  let commandWords = command.split(" ");
+  let iCommand = 0;
+  for (let inputWord of inputWords) {
+    let commandWord = commandWords[iCommand];
+    if (commandWord[0] == "{") {
+      // This is a placeholder
+      let argName = commandWord.substr(1, commandWord.length - 2);
+      console.log("detected variable:", argName, "=", inputWord);
+      if (args[argName]) {
+        args[argName] += inputWord;
+      } else {
+        args[argName] = inputWord;
+      }
+      iCommand++;
+    } else if (didYouMean2(inputWord, [ commandWord ])) {
+      // Word is part of the static command
+      console.log("detected command word:", inputWord, "=", commandWord);
+      iCommand++;
+    } else if (didYouMean2(inputWord, [ commandWords[iCommand + 1] ])) {
+      // Already next command word. Maybe there are superflous words in the sample command.
+      console.log("detected command word:", inputWord, "=", commandWords[iCommand + 1], ", while skipping:", commandWord);
+      iCommand++; // skip the superflous command word
+      iCommand++;
+    } else { // neither placeholder nor detected command word
+      console.log("skipping input word:", inputWord);
+      // Superflous word in input, e.g. "could you". Just skip it.
     }
-    //let similarity = stringSimilarity.findBestMatch(inputText, validValues).bestMatch.target;
-    //console.log("stringSimilarity:", similarity);
-    const startTime = new Date();
-    let match = didYouMean2(inputText, validValues);
-    console.log("did you mean:", match);
-    console.log("string matching took", (new Date() - startTime) + "ms");
-    return match;
   }
+  return args;
+}
+
+/**
+* @param inputText {string} user input, only the part for this variable
+* @param validValues {Array of string}
+* @returns {string} one line from validValues, the closest match
+*/
+function matchVariable(inputText, validValues) {
+  if (!validValues) {
+    return inputText;
+  }
+  //let similarity = stringSimilarity.findBestMatch(inputText, validValues).bestMatch.target;
+  //console.log("stringSimilarity:", similarity);
+  const startTime = new Date();
+  let match = didYouMean2(inputText, validValues);
+  console.log("did you mean:", match);
+  console.log("string matching took", (new Date() - startTime) + "ms");
+  if (!match) {
+    return inputText; // or throw?
+  }
+  return match;
 }
 
 
