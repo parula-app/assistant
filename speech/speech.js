b970e2a514140da4c2050d7e974c56bcaa28a2c9
Ben Bucksch <ben.bucksch@beonex.com>
2020-05-21 19:44:10 +0200

    Wake word: Move to speech/wakeword/

--- speech/speech.js
+++ speech/speech.js
@@ -1,5 +1,7 @@
 import * as _speechToText from './recognition/deepSpeech.js';
 import * as _textToSpeech from './tts/maryTTS.js';
+import * as _wakeword from './wakeword/bumblebee.js';
 
 export const speechToText = _speechToText;
 export const textToSpeech = _textToSpeech;
+export const wakeword = _wakeword;
