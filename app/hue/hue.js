52f1baf63e70b69c4ce8c3882236f17953bb7fff
Ben Bucksch <ben.bucksch@beonex.com>
2020-05-22 18:08:22 +0200

    Hue: Setup works

--- app/hue/hue.js
+++ app/hue/hue.js
@@ -2,7 +2,7 @@ import * as huePackage from 'node-hue-api';
 const hue = huePackage.default.v3;
 import { JSONApp } from '../../baseapp/JSONApp.js';
 import { getConfig } from '../../util/config.js';
-import { assert } from '../../util/util.js';
+import { wait, assert } from '../../util/util.js';
 
 export default class Hue extends JSONApp {
   constructor() {
@@ -23,15 +23,17 @@ export default class Hue extends JSONApp {
       return this._connection;
     }
     let config = getConfig().hue;
-    let username = config ? config.username : null;
-    let ipAddress = config ? config.ipAddress : null;
-    if (!username || !ipAddress) {
-      //{ username, ipAddress } = await this.setup(); -- JS parse error
-      let creds = await this.setup();
-      username = creds.username;
-      ipAddress = creds.ipAddress;
+    let hostname = config ? config.hostname : null;
+    let auth = config ? config.auth : null;
+    if (!hostname) {
+      hostname = await this.discoverBridge();
+      assert(hostname, "No Hue bridge found"); // already threw in discoverBridge()
     }
-    const authenticatedAPI = await hue.api.createLocal(ipAddress).connect(username);
+    if (!auth) {
+      //throw new Error("Hue bridge found, but not set up yet");
+      auth = await this.createCredentialsPolling(hostname, 300);
+    }
+    const authenticatedAPI = await hue.api.createLocal(hostname).connect(auth);
     // Test the connection
     const bridgeConfig = await authenticatedAPI.configuration.getConfiguration();
     console.log(`Connected to Hue bridge '${bridgeConfig.name}' at ${bridgeConfig.ipaddress}`);
@@ -42,27 +44,29 @@ export default class Hue extends JSONApp {
    * A one-time setup to find the bridge IP address,
    * create the access code to it, and save the data in the settings.
    *
-   * This needs user action to press a button on the Bridge.
-   * You need to instruct the user before you call this function.
+   * This needs user action in the process, @see createCredentials().
    *
    * @return {
-   *    ipAddress {string},
-   *    username {string},
+   *    hostname {string},
+   *    auth {string},
    * }
    * @throws Error when no bridges are found or access failed
    */
   async setup() {
-    let bridge = await this.discoverBridge();
-    return await this.createCredentials(bridge);
+    let hostname = await this.discoverBridge();
+    let auth = await this.createCredentialsPolling(hostname, 300);
+    return { hostname, auth };
   }
 
   /**
    * Find the IP address of the Hue bridge on the local network.
-   * @returns {string} ipAddress
+   * @returns {string} IP address
    * @throws Error when no bridges are found
    */
   async discoverBridge() {
-    let bridges = await hue.discovery.nupnpSearch();
+    console.time("hue-discovery")
+    let bridges = await hue.discovery.upnpSearch();
+    console.timeEnd("hue-discovery")
     // array of bridges that were found
     console.log(JSON.stringify(bridges, null, 2));
     bridges = bridges.filter(bridge => !bridge.error);
@@ -76,15 +80,19 @@ export default class Hue extends JSONApp {
    * Create a user account on the Hue bridge to allows us access.
    *
    * This needs user action to press a button on the Bridge.
+   * You need to instruct the user before you call this function.
    *
-   * @param ipAddress {string}
-   * @return {
-   *    ipAddress {string},
-   *    username {string},
+   * This function call must happen within seconds after the button press,
+   * so you probably need to call this function many times.
+   * @see createCredentialsPolling()
+   *
+   * @param hostname {string}
+   * @return {string} auth
+   * @throws NeedLinkButtonPress when the user didn't press the button yet
    * }
    */
-  async createCredentials(ipAddress) {
-    let unauthenticatedAPI = await hue.api.createLocal(ipAddress).connect();
+  async createCredentials(hostname) {
+    let unauthenticatedAPI = await hue.api.createLocal(hostname).connect();
     let deviceName = "pia"; // TODO add hostname
     try {
       let createdUser = await unauthenticatedAPI.users.createUser("pia-" + this.id, deviceName);
@@ -95,15 +103,39 @@ export default class Hue extends JSONApp {
       console.log(`Hue bridge user: ${createdUser.username}`);
       //console.log(`Hue bridge key: ${createdUser.clientkey}`); -- needed only for Streaming API
       console.log('************************************************************************\n');
-      return {
-        username: createdUser.username,
-        ipAddress: ipAddress,
-      };
+      return createdUser.username;
     } catch(ex) {
       if (ex.getHueErrorType && ex.getHueErrorType() === 101) {
-        throw new Error("The Link button on the Hue bridge was not pressed. Please press the Link button and try again.");
+        throw new NeedLinkButtonPress();
       } else {
         throw ex;
+      }
+    }
+  }
+
+  /**
+   * Keep trying to authenticate, until it works
+   * or `seconds` have passed.
+   *
+   * @see createCredentials()
+   *
+   * @param hostname {string}
+   * @param seconds {int} timeout, in seconds
+   * @return {string} auth
+   */
+  async createCredentialsPolling(hostname, seconds) {
+    const kInterval = 2; // in seconds
+    let tries = seconds / kInterval;
+    for (let i = 0; i < tries; i++) {
+      try {
+        await wait(kInterval);
+        return await this.createCredentials(hostname);
+      } catch (ex) {
+        if (ex instanceof NeedLinkButtonPress) {
+          console.log(i, ex.message);
+        } else {
+          throw ex;
+        }
       }
     }
   }
@@ -119,5 +151,12 @@ export default class Hue extends JSONApp {
     const kValidStates = [ "on", "off" ];
     assert(kValidStates.includes(args.State), "State must be either on or off");
 
+  }
+}
+
+class  NeedLinkButtonPress extends Error {
+  constructor() {
+    super("The Link button on the Hue bridge was not pressed. Please press the Link button and try again.");
+    this.name = "NeedLinkButtonPress";
   }
 }
