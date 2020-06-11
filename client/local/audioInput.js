3cf155435e636907f2ac0d4e9964ca262e365cd3
Ben Bucksch <ben.bucksch@beonex.com>
2020-06-11 02:39:37 +0200

    Use node-audiorecorder for audio input

--- client/local/audioInput.js
+++ client/local/audioInput.js
@@ -1,40 +1,8 @@
-import portAudio from 'naudiodon'; // for input -- output doesn't work for me
+import AudioRecorder from 'node-audiorecorder';
 import { speechToText } from '../../speech/speech.js'; // sample rate
 import { getConfig } from '../../util/config.js';
 
-/**
- * {Int} PortAudio device ID
- *    -1 = default device
- */
-var gDeviceID = -1;
-
 export async function load() {
-  getDevice();
-}
-
-function getDevice() {
-  let inputDevice = getConfig().audio.inputDevice;
-  for (let cur of portAudio.getDevices()) {
-    //if (cur.name.includes(device)) {
-    if (cur.name == inputDevice || cur.name.endsWith(" (" + inputDevice + ")")) {
-      console.log("using input device " + cur.name);
-      gDeviceID = cur.id;
-      return;
-    }
-  }
-}
-
-function listDevices() {
-  let inputDevice = getConfig().audio.inputDevice;
-  if (!inputDevice || inputDevice == -2) {
-    let devices = portAudio.getDevices();
-    //console.log(devices);
-    console.log("\nAudio devices:");
-    for (let device of devices) {
-      console.log(device.id + " = " + device.name);
-    }
-    throw new Error("Select an audio device for input and output");
-  }
 }
 
 /**
@@ -44,15 +12,18 @@ function listDevices() {
  *   Flows after this function returned.
  */
 export default function audioInput() {
-  let audioInputStream = new portAudio.AudioIO({
-    inOptions: {
-      channelCount: 1,
-      sampleFormat: portAudio.SampleFormat16Bit,
-      sampleRate: speechToText.sampleRate(),
-      // Use -1 to select the default device
-      deviceId: gDeviceID,
-      closeOnError: true,
-    }
-  });
-  return audioInputStream;
+  let audioRecorder = new AudioRecorder({
+    program: "sox", // "sox", "arecord" or "rec"
+    channels: 1,
+    bits: 16,
+    rate: speechToText.sampleRate(),
+    format: "S16_LE", // arecord only
+    encoding: "signed-integer", // sox only
+    endian: "little",
+    device: getConfig().audio.inputDevice, // arecord only
+    type: "raw",
+    silence: 0,
+  }, console);
+  audioRecorder.start();
+  return audioRecorder.stream();
 }
