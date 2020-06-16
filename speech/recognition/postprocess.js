8a94963cae64e7f972c713e17c85df47980c153b
Ben Bucksch <ben.bucksch@beonex.com>
2020-06-16 08:13:22 +0200

    Allow user to spell words with individual letters - not working

--- speech/recognition/postprocess.js
+++ speech/recognition/postprocess.js
@@ -5,3 +5,32 @@ export function textPostProcess(text) {
   text = wordsToNumbers(text) + ""; // leaves text as-is, only replaces numbers
   return text;
 }
+
+/**
+ * @param text {string} e.g. "r r r foo bar"
+ * @returns  {string} e.g. "rrr foo bar"
+ */
+function undoSpellAsLetters(text) {
+  let result = "";
+  let inSpelling = false;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == " " && // if " r ", whereas i is on the starting space
+        text.length > i + 1 && isLetter(text[i + 1]) &&
+        (text.length == i + 2 || text[i + 2] == " ")) {
+      if (inSpelling) {
+        // skip
+      } else {
+        result += text[i];
+        inSpelling = true;
+      }
+    } else {
+      result += text[i];
+      inSpelling = false;
+    }
+  }
+  return result;
+}
+
+function isLetter(letter) {
+  return letter.toUpperCase() != letter.toLowerCase();
+}
