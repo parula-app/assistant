c1bed7769b7682724f007953eab4c9e967634aec
Ben Bucksch <ben.bucksch@beonex.com>
2023-05-30 04:55:49 +0200

    Remove bash start code

--- client/Client.js
+++ client/Client.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
 'use strict';
 
 import IntentParser from '../intentParser/wildLeven/WildLevenIntentParser.js';
--- client/local/start.js
+++ client/local/start.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
 'use strict';
 
 import { LocalClient } from './LocalClient.js';
